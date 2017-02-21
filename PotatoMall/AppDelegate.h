6837a1f088e56b28deae8b2e1b8d951e59a21c5d
haitaowu <taotaowoo@gmail.com>
2017-02-21 21:02:11 +0800

    Initial Commit

---
+++ PotatoMall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PotatoMall
+//
+//  Created by taotao on 21/02/2017.
+//  Copyright © 2017 taotao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PotatoMall/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PotatoMall
+//
+//  Created by taotao on 21/02/2017.
+//  Copyright © 2017 taotao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
